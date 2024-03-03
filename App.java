c9ff2e2ffdaf3d1735004a440aab4f4ab3742894
Julian Farkas <julian.farkas.ger@gmail.com>
2024-03-03 18:43:21 +0100

    added comp. for (high)score

--- App.java
+++ App.java
@@ -11,6 +11,8 @@ public class App{
         //create and customize main window:
         JFrame RootFrame = new JFrame("Same Game");
         JPanel Scoreboard = new JPanel();
+            JLabel Score = new JLabel();
+            JLabel Highscore = new JLabel();
         JPanel PlayArea = new JPanel();
 
         RootFrame.setSize(800, 600);
@@ -18,10 +20,14 @@ public class App{
 
         //set layouts of components:
         RootFrame.setLayout(new BorderLayout());
+        Scoreboard.setLayout(new BoxLayout(Scoreboard, BoxLayout.X_AXIS));
         PlayArea.setLayout(new GridLayout());
         
         //adding components to parent components:
         RootFrame.add(Scoreboard, BorderLayout.NORTH);
+            Scoreboard.add(Score);
+            Scoreboard.add(Highscore);
+            
         RootFrame.add(PlayArea, BorderLayout.CENTER);
 
         //override default close operation so the window event listener can 
