c007df65c4a6b5ce26b1f321554e26ea01c62576
Julian Farkas <julian.farkas.ger@gmail.com>
2024-03-03 18:38:27 +0100

    added child components to frame

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
 public class App{
@@ -7,16 +10,25 @@ public class App{
 
         //create and customize main window:
         JFrame RootFrame = new JFrame("Same Game");
+        JPanel Scoreboard = new JPanel();
+        JPanel PlayArea = new JPanel();
 
         RootFrame.setSize(800, 600);
-        RootFrame.setLayout(null);
         RootFrame.setVisible(true);
+
+        //set layouts of components:
+        RootFrame.setLayout(new BorderLayout());
+        PlayArea.setLayout(new GridLayout());
+        
+        //adding components to parent components:
+        RootFrame.add(Scoreboard, BorderLayout.NORTH);
+        RootFrame.add(PlayArea, BorderLayout.CENTER);
 
         //override default close operation so the window event listener can 
         //handle it and do some cleanup before closing:
         RootFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
-        // add event listeners to elements:
+        // add event listeners to components:
         RootFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing (WindowEvent ev) {
                 RootFrame.dispose();
