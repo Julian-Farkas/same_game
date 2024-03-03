cd571514d437888eaf24d65574b03e6457e63892
Julian Farkas <julian.farkas.ger@gmail.com>
2024-03-03 21:40:22 +0100

    block generation and scorefix

--- App.java
+++ App.java
@@ -6,12 +6,20 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.*;
 
-public class App{
+
+public class App {
 
     // local variables off app:
 
     private int score = 0;
     private int highscore = 0;
+
+    //array for color creation:
+    private final String[] colors = {"0xffffff", "0xff0000", "0x00ff00", "0x0000ff", "0xff8800", "0xaa0066"}; 
+
+    // variables for play area:
+    protected int[][] area = new int[10][10];
+    protected JPanel[][] Area = new JPanel[10][10];
 
     private Dimension screenSize = new Dimension(800,600);
 
@@ -24,9 +32,53 @@ public class App{
         return this.highscore;
     }
 
+    public void setScreenSize(Dimension newSize) {
+        this.screenSize = newSize;
+    }
 
 
     //other methods:
+
+
+    /*
+     * refills the area when game starts or the board is
+     * empty. First the area-array gets filles with numbers in the range of 
+     * the colors-array.
+     */
+    public void fillPlayArea (JPanel RootPanel, int[][] area, JPanel[][] PlayArea) {
+
+        for (int row = 0; row < 10; ++row) {
+            for (int col = 0; col < 10; ++col) {
+                area[row][col] = (int) (Math.random() * 5 + 1);
+            }
+        }
+        draw(RootPanel, area, PlayArea);
+    }
+
+    //function to paint the play area:
+    public void draw (JPanel RootPanel, int[][] area, JPanel[][] PlayArea) {
+
+        Color cellColor = null;
+        
+        for (int row = 0; row < 10; ++row) {
+            for (int col = 0; col < 10; ++col) {
+
+                /*
+                 * get the cell color by first get the string name of the color 
+                 * determined by the area array, then the string gets decoded by the
+                 * decode funcion of Color class:
+                 */
+                cellColor = Color.decode( colors[ area[row][col] ] );
+
+                Area[row][col] = new JPanel();
+                Area[row][col].setSize(new Dimension( screenSize.width/10, (screenSize.height * 9 / 100) ) );
+                Area[row][col].setBackground(cellColor);
+                RootPanel.add(Area[row][col]);
+            }
+        }
+    }
+
+
 
     public static void main(String[] args) {
 
@@ -36,24 +88,23 @@ public class App{
         JPanel Scoreboard = new JPanel();
             JLabel Score = new JLabel();
             JLabel Highscore = new JLabel();
+        JPanel PlayAreaConstraints = new JPanel();
         JPanel PlayArea = new JPanel();
+    
 
         RootFrame.setVisible(true);
-        Scoreboard.setBackground(Color.ORANGE);
-        Score.setBackground(Color.RED);
-        Highscore.setBackground(Color.BLUE);
-        PlayArea.setBackground(Color.GREEN);
 
         //set layouts of components:
         RootFrame.setLayout(new BorderLayout());
         Scoreboard.setLayout(new BoxLayout(Scoreboard, BoxLayout.X_AXIS));
-        PlayArea.setLayout(new GridLayout());
+        PlayArea.setLayout(new GridLayout(10,10));
 
         // intitial size settings of components:
         RootFrame.setSize(800, 600);
         Score.setPreferredSize(new Dimension(app.screenSize.width/2, app.screenSize.height/10));
         Highscore.setPreferredSize(new Dimension(app.screenSize.width/2, app.screenSize.height/10));
-        System.out.println(app.screenSize.width/2 + " " + app.screenSize.height/10);
+        PlayAreaConstraints.setSize(new Dimension( app.screenSize.width, app.screenSize.height * 8 / 10 ) );
+        PlayArea.setSize(new Dimension( app.screenSize.width, app.screenSize.height * 8 / 10 ) );
 
         //set text algnment of (high)score:
         Score.setVerticalAlignment(SwingConstants.CENTER);
@@ -65,9 +116,10 @@ public class App{
         RootFrame.add(Scoreboard, BorderLayout.NORTH);
             Scoreboard.add(Score);
             Scoreboard.add(Highscore);
-            Scoreboard.add(Box.createVerticalGlue());
 
         RootFrame.add(PlayArea, BorderLayout.CENTER);
+            //PlayAreaConstraints.add(PlayArea);
+        PlayArea.setBackground(Color.LIGHT_GRAY);
         
 
         //initially write score and highscore:
@@ -85,5 +137,19 @@ public class App{
                 return;
             }
         });
+
+        //resize all childs of main window:
+        RootFrame.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent ev) {
+                //reset the screen size:
+                app.setScreenSize(RootFrame.getSize());
+
+                Score.setPreferredSize(new Dimension(app.screenSize.width/2, app.screenSize.height/10));
+                Highscore.setPreferredSize(new Dimension(app.screenSize.width/2, app.screenSize.height/10));
+            }
+        });
+
+        app.fillPlayArea(PlayArea, app.area, app.Area);
+        RootFrame.repaint();
     }
 }
