572408abbbf8fe2ad4dc55af1698b9242b7e5d0a
Julian Farkas <julian.farkas.ger@gmail.com>
2024-03-03 18:25:14 +0100

    add window listener for closing app

--- App.java
+++ App.java
@@ -1,8 +1,28 @@
+import javax.swing.*;
+import java.awt.event.*;
 
-
-public class App {
+public class App{
 
     public static void main(String[] args) {
+
+        //create and customize main window:
+        JFrame RootFrame = new JFrame("Same Game");
+
+        RootFrame.setSize(800, 600);
+        RootFrame.setLayout(null);
+        RootFrame.setVisible(true);
+
+        //override default close operation so the window event listener can 
+        //handle it and do some cleanup before closing:
+        RootFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+        // add event listeners to elements:
+        RootFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing (WindowEvent ev) {
+                RootFrame.dispose();
+                return;
+            }
+        });
         
     }
 }
